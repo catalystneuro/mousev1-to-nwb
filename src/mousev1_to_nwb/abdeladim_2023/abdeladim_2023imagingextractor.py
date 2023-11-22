76c8f6120e4c1a57ea31e7fb9a989bbfd11c5d9d
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2023-11-22 15:34:55 +0100

    minor fixes from reviews

--- src/mousev1_to_nwb/abdeladim_2023/abdeladim_2023imagingextractor.py
+++ src/mousev1_to_nwb/abdeladim_2023/abdeladim_2023imagingextractor.py
@@ -1,6 +1,5 @@
 from typing import Optional, Tuple, List, Iterable
 from pathlib import Path
-import glob
 from roiextractors.extractors.tiffimagingextractors.scanimagetiffimagingextractor import (
     ScanImageTiffMultiPlaneImagingExtractor,
     ScanImageTiffSinglePlaneImagingExtractor,
@@ -11,6 +10,7 @@ from roiextractors.multiimagingextractor import MultiImagingExtractor
 
 from roiextractors.extraction_tools import PathType, FloatType, ArrayType, DtypeType, get_package
 import numpy as np
+from natsort import natsorted
 
 
 class Abdeladim2023MultiPlaneImagingExtractor(MultiImagingExtractor):
@@ -26,8 +26,8 @@ class Abdeladim2023MultiPlaneImagingExtractor(MultiImagingExtractor):
         channel_name: Optional[str] = None,
     ) -> None:
         self.folder_path = Path(folder_path)
-        tif_file_paths = sorted(glob.glob(f"{self.folder_path}/*.tif"))
-        assert tif_file_paths, f"The TIF image files are missing from '{folder_path}'."
+        tif_file_paths = natsorted(self.folder_path.glob("*.tif"))
+        assert tif_file_paths, f"The TIF image files are missing from '{self.folder_path}'."
 
         imaging_extractors = [
             ScanImageTiffMultiPlaneImagingExtractor(file_path=file_path, channel_name=channel_name)
@@ -37,7 +37,7 @@ class Abdeladim2023MultiPlaneImagingExtractor(MultiImagingExtractor):
         super().__init__(imaging_extractors=imaging_extractors)
 
 
-class Abdeladim2023SinglePlaneImagingExtractor(ImagingExtractor):
+class Abdeladim2023SinglePlaneImagingExtractor(MultiImagingExtractor):
     """Specialized extractor for Abdeladim2023 conversion project: reading ScanImage .tif files chunked over time"""
 
     extractor_name = "Abdeladim2023SinglePlaneImagingExtractor"
@@ -51,7 +51,7 @@ class Abdeladim2023SinglePlaneImagingExtractor(ImagingExtractor):
         plane_name: str,
     ) -> None:
         self.folder_path = Path(folder_path)
-        tif_file_paths = sorted(glob.glob(f"{self.folder_path}/*.tif"))
+        tif_file_paths = natsorted(self.folder_path.glob("*.tif"))
         assert tif_file_paths, f"The TIF image files are missing from '{folder_path}'."
 
         imaging_extractors = [
@@ -61,53 +61,5 @@ class Abdeladim2023SinglePlaneImagingExtractor(ImagingExtractor):
             for file_path in tif_file_paths
         ]
 
-        self.imaging_extractor = MultiImagingExtractor(imaging_extractors)
-        times = self.imaging_extractor._get_times()
-        self.set_times(times=times)
+        super().__init__(imaging_extractors=imaging_extractors)
 
-    def get_frames(self, frame_idxs: ArrayType) -> np.ndarray:
-        """Get specific video frames from indices (not necessarily continuous).
-
-        Parameters
-        ----------
-        frame_idxs: array-like
-            Indices of frames to return.
-
-        Returns
-        -------
-        frames: numpy.ndarray
-            The video frames.
-        """
-        return self.imaging_extractor.get_frames(frame_idxs=frame_idxs)
-
-    def get_num_frames(self) -> int:
-        return self.imaging_extractor.get_num_frames()
-
-    def get_video(self, start_frame: Optional[int] = None, end_frame: Optional[int] = None) -> np.ndarray:
-        """Get the video frames.
-
-        Parameters
-        ----------
-        start_frame: int, optional
-            Start frame index (inclusive).
-        end_frame: int, optional
-            End frame index (exclusive).
-
-        Returns
-        -------
-        video: numpy.ndarray
-            The video frames.
-        """
-        return self.imaging_extractor.get_video(start_frame=start_frame, end_frame=end_frame)
-
-    def get_channel_names(self):
-        return self.imaging_extractor.get_channel_names()
-
-    def get_image_size(self):
-        return self.imaging_extractor.get_image_size()
-
-    def get_num_channels(self):
-        return self.imaging_extractor.get_num_channels()
-
-    def get_sampling_frequency(self):
-        return self.imaging_extractor.get_sampling_frequency()
